5ae2b972c8e896ca8f23c3a6bcad7bfba0ec65b9
Guotong Liao <815190191@qq.com>
2022-12-11 10:50:23 -0800

    refactor: optional chaining

--- content-script.js
+++ content-script.js
@@ -2,17 +2,16 @@ chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
   const { enable, rule } = message;
 
   if (enable) {
-
+    // remove the style if it already exists
     const style = document.getElementById('visualizerEnabled');
-    if (style) style.parentNode.removeChild(style);
-
+    style?.parentNode.removeChild(style);
+    // create a new style element
     const newStyle = document.createElement('style');
     newStyle.setAttribute('id', 'visualizerEnabled');
     newStyle.innerHTML = rule;
     document.head.appendChild(newStyle);
   } else {
     const style = document.getElementById('visualizerEnabled');
-    if (!style) return;
-    style.parentNode.removeChild(style);
+    style?.parentNode.removeChild(style);
   }
 });
